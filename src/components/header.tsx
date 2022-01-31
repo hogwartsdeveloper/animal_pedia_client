1c3f964dd182056ceab8370ecd1489c3c654b5d1
Zhannur <jannuraidynuly@gmail.com>
2022-01-31 15:26:47 +0500

    searchInput component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,38 @@
 import { FC } from "react";
-import { Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
+import { Poppins_900Black, useFonts } from "@expo-google-fonts/poppins";
+import AppLoading from "expo-app-loading";
+import SearchInput from "./searchInput";
 
 const Header: FC = () => {
+    const [fontsLoaded] = useFonts({
+        Poppins_900Black
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />
+    }
+
     return (
-        <View>
-            <Text>Animal pediA</Text>
-            <TextInput />
+        <View style={styles.container}>
+            <Text style={styles.logo}>Animal pediA</Text>
+            <SearchInput />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column',
+        marginTop: 40,
+        padding: 15
+    },
+    logo: {
+        fontFamily: 'Poppins_900Black',
+        fontSize: 25,
+        fontWeight: '900',
+        marginBottom: 10
+    }
+})
 
 export default Header;
---
+++ src/components/searchInput.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons"
+
+const SearchInput: FC = () => {
+    return (
+        <View style={styles.container}>
+            <AntDesign style={styles.searchIcon} name="search1"/>
+            <TextInput style={styles.input} placeholder="Поиск по Animal pedia" />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: '#d1f1db',
+        borderRadius: 5,
+        paddingVertical: 3,
+        paddingHorizontal: 5
+    },
+    searchIcon: {
+        fontSize: 20,
+        color: '#f3c8df',
+        marginRight: 5
+    },
+    input: {
+        fontSize: 16
+    }
+})
+
+export default SearchInput;
