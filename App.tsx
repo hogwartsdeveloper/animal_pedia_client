174744bc31a54cb37686bce3abeafe3f5bbd6e5d
Zhannur <jannuraidynuly@gmail.com>
2022-02-05 10:18:01 +0500

    ignore ol API use bug

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import { Provider } from "react-redux"
 import { store } from "./src/store";
 import MainNav from "./src/navigation/mainNav";
+import { LogBox } from "react-native";
 
 export default function App() {
+  LogBox.ignoreLogs([
+    "[react-native-gesture-handler] Seems like you\'re using an old API with gesture components, check out new Gestures system!",
+  ]);
+
   return (
     <Provider store={store}>
         <MainNav />
